821d13b458d5ae8a7311b55c5e0414336dc7c886
ajai1422 <ajaiakshai1232@gmail.com>
2026-02-27 16:04:08 +0530

    update

--- contexts/LanguageContext.tsx
+++ contexts/LanguageContext.tsx
@@ -5,21 +5,13 @@ import ta from '../locales/ta.json';
 import hi from '../locales/hi.json';
 import as from '../locales/as.json';
 import bn from '../locales/bn.json';
-import brx from '../locales/brx.json';
-import doi from '../locales/doi.json';
 import gu from '../locales/gu.json';
 import kn from '../locales/kn.json';
-import kok from '../locales/kok.json';
-import ks from '../locales/ks.json';
-import mai from '../locales/mai.json';
 import ml from '../locales/ml.json';
-import mni from '../locales/mni.json';
 import mr from '../locales/mr.json';
 import ne from '../locales/ne.json';
 import od from '../locales/od.json';
 import pa from '../locales/pa.json';
-import sa from '../locales/sa.json';
-import sat from '../locales/sat.json';
 import te from '../locales/te.json';
 import ur from '../locales/ur.json';
 
@@ -29,21 +21,13 @@ const LOCALE_MAP: Record<string, any> = {
     [Language.HINDI]: hi,
     [Language.ASSAMESE]: as,
     [Language.BENGALI]: bn,
-    [Language.BODO]: brx,
-    [Language.DOGRI]: doi,
     [Language.GUJARATI]: gu,
     [Language.KANNADA]: kn,
-    [Language.KASHMIRI]: ks,
-    [Language.KONKANI]: kok,
-    [Language.MAITHILI]: mai,
     [Language.MALAYALAM]: ml,
-    [Language.MANIPURI]: mni,
     [Language.MARATHI]: mr,
     [Language.NEPALI]: ne,
     [Language.ODIA]: od,
     [Language.PUNJABI]: pa,
-    [Language.SANSKRIT]: sa,
-    [Language.SANTALI]: sat,
     [Language.TELUGU]: te,
     [Language.URDU]: ur,
 };
