f0a78945efb7a3fd607bc9f11e8efc2cd5488422
ajai1422 <ajaiakshai123@gmail.com>
2026-06-24 10:16:07 +0530

    refactor: repository cleanup and structure optimization

--- backend/scratch/run_migration.js
+++
@@ -1,9 +0,0 @@
-import { initializeAuthTables } from "../utils/db-setup.js";
-
-async function main() {
-    console.log("Starting migration run...");
-    await initializeAuthTables();
-    console.log("Migration run finished.");
-    process.exit(0);
-}
-main();
