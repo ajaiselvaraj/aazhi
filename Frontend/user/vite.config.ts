946b435805450aef579ea9fc6b189b4abd28d4cd
ajai1422 <ajaiakshai123@gmail.com>
2026-06-28 15:18:35 +0530

    gemini bck

--- Frontend/user/vite.config.ts
+++ Frontend/user/vite.config.ts
@@ -39,8 +39,6 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.VITE_LOCAL_IP': JSON.stringify(getLocalIP()) // Export local IP
       },
       resolve: {
